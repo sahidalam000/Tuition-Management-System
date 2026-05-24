2a9a831bd3cc3021b2e0f84712c6b705357e04ee
Sahid Alam <sahidalam538@gmail.com>
2026-05-24 19:52:49 +0530

    Fix hero header

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -79,16 +79,7 @@ const Hero = () => {
         </motion.p>
 
         {/* ✅ HEADING (ONE TIME TYPING) */}
-        <h1 className="text-4xl
-sm:text-5xl
-md:text-6xl
-font-bold
-text-white
-mb-5
-leading-tight
-max-w-[320px]
-sm:max-w-[700px]
-mx-auto">
+        <h1 className="text-4xl md:text-6xl font-bold text-white mb-5 leading-tight">
           {headingText}
         </h1>
 
